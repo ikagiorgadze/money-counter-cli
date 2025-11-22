f86473149a98cc03e4bfdc23bb63b10b7a1e970f
Irakli Giorgadze <ik48giorgadze@gmail.com>
2025-11-22 14:48:24 +0400

    Added function to create/validate daily csv files. Removed creation of old file type. Csv files are now created daily and document daily spending in files named: {date}.csv. Each row has: Amount Spent, Description, Balance.

--- data_processor.py
+++ data_processor.py
@@ -1,6 +1,31 @@
 from datetime import date
 import csv
 
+
+today = date.today().strftime("%d-%m-%Y")
+daily_csv_path = f"/data/daily-spendings/{today}.csv"
+
+
+def print_error(error: Exception) -> None:
+    print(f"Error: {error}")
+
+
+def create_daily_csv():
+    first_row = ["Amount Spent", "Description", "Balance"]
+    with open(daily_csv_path, "w") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(first_row)
+
+
+def validate_daily_csv() -> None:
+    try:
+        with open(daily_csv_path, "r") as csv_file:
+            reader = csv.reader(csv_file)
+            reader[0]
+    except FileNotFoundError:
+        create_daily_csv()
+
+
 def validate_amount(amount) -> float:
     try:
         amount = float(amount)
@@ -8,17 +33,34 @@ def validate_amount(amount) -> float:
             raise ValueError("Input must be larger or equal to 0")
 
         return amount
+    except ValueError as e:
+        print_error(e)
 
-    except ValueError:
-        print("Input the initial amount as a number")
+
+def validate_description(description) -> str:
+    try:
+        if not isinstance(description, str):
+            raise ValueError("Input must be a string")
+        if len(description) < 3:
+            raise ValueError("Input string must be longer than 3 characters")
+
+        return description
+    except ValueError as e:
+        print_error(e)
 
 
-def create_initial_csv(initial_amount: float | int) -> None:
-    amount = validate_amount(initial_amount)
+def add_spending_to_daily_csv(
+    amount_spent: float | int,
+    description: str,
+    balance: float | int,
+) -> None:
+    validate_daily_csv()
+    amount = validate_amount(amount_spent)
+    description = validate_description
+    balance = validate_amount(balance)
 
-    header_rows = ["Date", "Amount Spent", "Remainder"]
-    first_row = [date.today().strftime("%d-%m-%Y"), "0", str(initial_amount)]
+    row = [str(amount), description, str(balance - amount)]
 
-    with open(f"data/daily-spending.csv", "w") as csv_file:
-        csv_file.writelines([",".join(header_rows), "\n", ",".join(first_row)])
-        # TODO - Write this functionality using csv.writer instead.
+    with open(daily_csv_path, "w") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(row)
